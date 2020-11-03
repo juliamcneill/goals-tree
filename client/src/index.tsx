e16e00b372b9901dd157ab2c60de1942e9e1d94d
Julia McNeill <juliammcneill@gmail.com>
2020-11-03 14:57:14 -0500

    Adjust grid to autofill goals to top

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -17,30 +17,40 @@ class App extends React.Component<{}, MyState> {
           <h1>Goals Tree</h1>
         </header>
         <div className="goals">
-          <div className="goal life-goal">Example life goal</div>
-          <div className="goal life-goal">Example life goal</div>
-          <div className="goal year-goal">Example year goal</div>
-          <div className="goal year-goal">Example year goal</div>
-          <div className="goal year-goal">Example year goal</div>
-          <div className="goal year-goal">Example year goal</div>
-          <div className="goal month-goal">Example month goal</div>
-          <div className="goal month-goal">Example month goal</div>
-          <div className="goal month-goal">Example month goal</div>
-          <div className="goal month-goal">Example month goal</div>
-          <div className="goal month-goal">Example month goal</div>
-          <div className="goal week-goal">Example week goal</div>
-          <div className="goal week-goal">Example week goal</div>
-          <div className="goal week-goal">Example week goal</div>
-          <div className="goal week-goal">Example week goal</div>
-          <div className="goal week-goal">Example week goal</div>
-          <div className="goal week-goal">Example week goal</div>
-          <div className="goal day-goal">Example day goal</div>
-          <div className="goal day-goal">Example day goal</div>
-          <div className="goal day-goal">Example day goal</div>
-          <div className="goal day-goal">Example day goal</div>
-          <div className="goal day-goal">Example day goal</div>
-          <div className="goal day-goal">Example day goal</div>
-          <div className="goal day-goal">Example day goal</div>
+          <div className="goal-column">
+            <div className="goal life-goal">Example life goal</div>
+            <div className="goal life-goal">Example life goal</div>
+          </div>
+          <div className="goal-column">
+            <div className="goal year-goal">Example year goal</div>
+            <div className="goal year-goal">Example year goal</div>
+            <div className="goal year-goal">Example year goal</div>
+            <div className="goal year-goal">Example year goal</div>
+          </div>
+          <div className="goal-column">
+            <div className="goal month-goal">Example month goal</div>
+            <div className="goal month-goal">Example month goal</div>
+            <div className="goal month-goal">Example month goal</div>
+            <div className="goal month-goal">Example month goal</div>
+            <div className="goal month-goal">Example month goal</div>
+          </div>
+          <div className="goal-column">
+            <div className="goal week-goal">Example week goal</div>
+            <div className="goal week-goal">Example week goal</div>
+            <div className="goal week-goal">Example week goal</div>
+            <div className="goal week-goal">Example week goal</div>
+            <div className="goal week-goal">Example week goal</div>
+            <div className="goal week-goal">Example week goal</div>
+          </div>
+          <div className="goal-column">
+            <div className="goal day-goal">Example day goal</div>
+            <div className="goal day-goal">Example day goal</div>
+            <div className="goal day-goal">Example day goal</div>
+            <div className="goal day-goal">Example day goal</div>
+            <div className="goal day-goal">Example day goal</div>
+            <div className="goal day-goal">Example day goal</div>
+            <div className="goal day-goal">Example day goal</div>
+          </div>
         </div>
       </>
     );
